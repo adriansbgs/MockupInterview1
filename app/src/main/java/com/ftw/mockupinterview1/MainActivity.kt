4ddde6c7936484d418979836a4ec407580baa164
Adrian Sabagus <low.adrian71@gmail.com>
2020-07-28 17:17:10 +0700

    Tambah @JvmStatic dan Companion Object agar bisa running di console

--- app/src/main/java/com/ftw/mockupinterview1/MainActivity.kt
+++ app/src/main/java/com/ftw/mockupinterview1/MainActivity.kt
@@ -3,42 +3,50 @@ package com.ftw.mockupinterview1
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), InterfaceDice.View {
-    private var userList = mutableListOf<User>()
-    private var userName = ""
-    private var presenter = PresenterDice(this)
-    private var currentScore = 0
-    private var totalPlayers = 0
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+    companion object : InterfaceDice.View {
 
-        println("Masukkan jumlah pemain")
-        totalPlayers = readLine()!!.toInt()
+        private var userList = mutableListOf<User>()
+        private var userName = ""
+        private var presenter = PresenterDice(this)
+        private var currentScore = 0
+        private var totalPlayers = 0
 
-        for (i in 0..totalPlayers) {
-            println("Masukan nama kamu")
-            userName = readLine().toString()
-            val score = play()
-            println("Halo $userName")
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Masukkan jumlah pemain")
+            totalPlayers = readLine()!!.toInt()
+
+            for (i in 1..totalPlayers) {
+                currentScore = 0
+                println("Masukan nama kamu")
+                userName = readLine().toString()
+                play()
+                println("Halo $userName")
+            }
+
+            val sortedList = userList.sortedByDescending {
+                it.score
+            }
+            println(sortedList[0].nama + " adalah pemenang dengan skor " + sortedList[0].score)
+            println("=========================================")
+            println("Berikut datanya:")
+            println(sortedList.forEach {
+                println("${it.nama} ${it.score}")
+            })
         }
 
-        val sortedList = userList.sortedBy {
-            it.score
+        private fun play() {
+
+            for (i in 0..4) {
+                val rands = (1..6).random()
+                presenter.process(rands)
+            }
+            userList.add(User(userName, currentScore))
         }
-        println(sortedList[0].nama + " adalah pemenang dengan skor " + sortedList[0].score)
-    }
 
-    private fun play() {
-        val rands = (1..6).random()
-
-        for (i in 0..4) {
-            presenter.process(rands)
+        override fun scoreResult(score: Int) {
+            currentScore += score
         }
-        userList.add(User(userName, currentScore))
-    }
-
-    override fun scoreResult(score: Int) {
-        currentScore += score
     }
 }
--- app/src/main/java/com/ftw/mockupinterview1/PresenterDice.kt
+++ app/src/main/java/com/ftw/mockupinterview1/PresenterDice.kt
@@ -4,9 +4,9 @@ class PresenterDice (val view: InterfaceDice.View): InterfaceDice.Presenter {
 
     override fun process(currentDice: Int) {
         var score = 0
-        if (currentDice % 2 == 0) {
-            score = -3
-        } else score = 5
+        score = if (currentDice % 2 == 0) {
+            -3
+        } else 5
         view.scoreResult(score)
     }
 }
